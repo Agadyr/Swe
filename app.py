5710e2909e2315d7e530cf0e60e8cee0e83859ca
Agadyr <kairambekovmadi@gmail.com>
2024-12-24 09:15:17 +0500

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask
 from flask_migrate import Migrate
 from flask_cors import CORS
@@ -6,19 +5,22 @@ from config import Config
 from models import db
 from routes import routes
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     CORS(app)
     db.init_app(app)
-    
+
     migrate = Migrate(app, db)
 
     app.register_blueprint(routes)
 
     return app
 
+
+app = create_app()  # Initialize the app here
+
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True, use_reloader=True,port=5000,host="0.0.0.0")
+    app.run(debug=True, use_reloader=True, port=5000, host="0.0.0.0")
